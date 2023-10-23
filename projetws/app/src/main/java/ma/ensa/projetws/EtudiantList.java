aa51d6dbe052e3305de3a32f53cd742198558ecf
Wiam Merjane <merjane.wiam6@gmail.com>
2023-10-23 22:26:00 +0100

    first commit

---
+++ projetws/app/src/main/java/ma/ensa/projetws/EtudiantList.java
@@ -0,0 +1,14 @@
+package ma.ensa.projetws;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EtudiantList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_etudiant_list);
+    }
+}
